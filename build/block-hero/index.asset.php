1d8c76dc7020b3bd109d4bbddd9a8875fef5f927
maxamuz <laptevx@gmail.com>
2026-07-28 12:21:03 +0300

    Правыки стилей блока

--- build/block-hero/index.asset.php
+++ build/block-hero/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4164d880777b7fae18bd');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'b9fcc0be98425efe64f1');
